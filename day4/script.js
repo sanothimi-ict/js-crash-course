29e6e76ceb33b0fa5ca465eac8c9c642dfae97c0
Ramesh Syangtan <rameshsyangtan92@gmail.com>
2020-01-17 15:19:44 +0545

    day4

---
+++ day4/script.js
@@ -0,0 +1,27 @@
+
+// const request = new XMLHttpRequest();
+
+// request.addEventListener('load', handleResponse);
+
+// function handleResponse () {
+//   const responseObj = JSON.parse(request.response);
+//   console.log(responseObj);
+// }
+
+// request.open('get', `http://192.168.43.247:8888/api/notice-list`);
+// request.send();
+
+  // fetch('http://192.168.43.247:8888/api/js-course-students')
+  //   .then(function (blob) {
+  //     return blob.json();
+  //   })
+  //   .then(function (response) {
+  //     let html = "";
+  //     response.forEach(function (stu) {
+  //       html = html + `<li><strong>${stu.name}</strong>, ${stu.age}</li>`;
+  //     });
+
+  //     const ul = document.querySelector("ul");
+  //     ul.innerHTML = html;
+  //   });
+
